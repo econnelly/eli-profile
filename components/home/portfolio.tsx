a63a41b9550a9de48a20e0989e87f5b8639b0b43
Eli <eli@eliconnelly.com>
2023-10-15 22:38:44 -0700

    Add the ability to close portfolio project dialog via keyboard

--- components/home/portfolio.tsx
+++ components/home/portfolio.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import ProjectData from "../../_data/projects.json"
-import ReactModal from "react-modal";
 
 const Portfolio = () => {
     const projects = ProjectData.projects
@@ -25,45 +24,61 @@ const Portfolio = () => {
         return (
             <>
                 <div
-                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none project-modal"
+                    className={"justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none project-modal bg-black bg-opacity-75 ease-linear transition-all duration-500"}
+                    onClick={(e) => {
+                        e.preventDefault()
+                        closeModal()
+                    }}
                 >
-                    <div className="relative w-auto my-6 mx-auto max-w-6xl">
+                    <div className={"relative w-auto my-6 mx-auto max-w-6xl"} onClick={(e) => {e.preventDefault()}}>
                         {/*content*/}
-                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                        <div className={"border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"}>
                             {/*header*/}
-                            <div className="flex items-start justify-between p-0 rounded-t">
+                            <div className={"flex items-start justify-between p-0 rounded-t"}>
                                 <img src={`assets/images/portfolio/${project.image}`} alt="" className={"center w-full"}/>
                             </div>
-                            <div className="flex items-start justify-between p-5 rounded-t">
-                                <h3 className="text-3xl font-semibold">
+                            <div className={"flex items-start justify-between p-5 rounded-t"}>
+                                <h3 className={"text-3xl font-semibold"}>
                                     {project.title}
                                 </h3>
                             </div>
                             {/*body*/}
-                            <div className="relative p-6 flex-auto">
-                                <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                            <div className={"relative p-6 flex-auto"}>
+                                <p className={"my-4 text-blueGray-500 text-lg leading-relaxed"}>
                                     {project.description}
                                 </p>
                             </div>
                             {/*footer*/}
-                            <div className="flex items-center justify-end px-6 py-2 rounded-b w-full">
-                                <a href={"#"} className="align-left background-transparent font-bold uppercase px-6 py-2 text-base outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" onClick={(e) => {
+                            <div className={"flex items-center justify-end px-6 py-2 rounded-b w-full"}>
+                                <a href={"#"} className={"align-left background-transparent font-bold uppercase px-6 py-2 text-base outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"} onClick={(e) => {
                                     e.preventDefault()
                                     closeModal()
                                 }}>
                                     Close
                                 </a>
-                                <a href={project.url} target={"_blank"} className="background-transparent font-bold uppercase px-6 py-2 text-base outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" >
+                                <a href={project.url} target={"_blank"} className={"background-transparent font-bold uppercase px-6 py-2 text-base outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"} >
                                     Project Link
                                 </a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                <div className={"opacity-25 fixed inset-0 z-40 bg-black"}></div>
             </>
         )
     }
+
+    useEffect(() => {
+        const close = (e:KeyboardEvent) => {
+            if(e.key === 'Escape'){
+                closeModal()
+            }
+
+            console.log({key: e.key, open: isOpen})
+        }
+        window.addEventListener('keydown', close)
+        return () => window.removeEventListener('keydown', close)
+    },[])
 
     useEffect(() => {
         console.log(`Setting isOpen to ${isOpen}`)
@@ -72,8 +87,8 @@ const Portfolio = () => {
     return (
         <section id="portfolio" className={"s-portfolio target-section"}>
 
-            <div className="row s-portfolio__header">
-                <div className="column large-12">
+            <div className={"row s-portfolio__header"}>
+                <div className={"column large-12"}>
                     <h3>{ProjectData.description}</h3>
                 </div>
             </div>
