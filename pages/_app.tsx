1adcd89b9ac4ce0415b5f653c9abd0a515e4e938
Eli <eli@eliconnelly.com>
2024-12-18 21:41:20 -0800

    Updating _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/index.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=XXXXXXXXX`}
+        />
+        <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXX', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+        />
+      <Component {...pageProps} />
+        </>
+  )
 }
